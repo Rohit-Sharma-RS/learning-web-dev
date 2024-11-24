4b02ffd02339bb5e8c4921f4a00de5b969ff51fe
Rohit-Sharma-RS <rohitrnc5458@gmail.com>
2024-11-24 20:52:00 +0530

    added sounds with keyboard for drum kit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,8 +1,97 @@
-for(i=0; i<=document.querySelectorAll(".drum").length; i++){
+for(i=0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
 }
 
 function handleClick(){
-    alert("I got clicked!");
+    buttons = this.innerHTML
+    
+    switch (buttons){
+        case 'w':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        default:
+            console.log("Invalid");
+
+    }
+}
+
+function playSound(){
+    var audio4 = new Audio("tom-2.mp3");
+    audio4.play();
+}   
+
+document.addEventListener("keydown", function(event){
+    playSound(event);
+});
+
+function playSound(event){
+    key = event.key;
+    switch (key){
+        case 'w':
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        default:
+            console.log("Invalid");
+
+    }
+}
+
+
+function buttonAnimation(current_key){
+    var active_button = document.querySelectorAll("." + current_key);
+    active_button.classList.add("pressed");
+    setTimeout(() => {
+        
+    }, timeout);
 }
