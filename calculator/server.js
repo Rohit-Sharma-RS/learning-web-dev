c7d3cbf30fbe3b1a10fef5692d481811a7ebb560
Rohit-Sharma-RS <rohitrnc5458@gmail.com>
2024-12-10 19:41:26 +0530

    new changes huh

--- calculator/server.js
+++ calculator/server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    console.log(req)
-    res.send("index.html")
+    res.sendFile(__dirname+"/index.html");
 })
 
 app.listen(3000, function(){
-    console.log("Server running on port 3000");
+    console.log("Server running on port http://127.0.0.1:3000/");
 });
